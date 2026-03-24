b8893262317b02e199cc9f0a912d0fc5d73ef1fd
caldecid <caldecid@gmail.com>
2026-03-24 16:07:41 -0300

    phylogenetic consistency. Not prunning the tree for each region

--- Scripts/02_phylo_manipulation.R
+++ Scripts/02_phylo_manipulation.R
@@ -1,67 +1,37 @@
-# Phylogenetic manipulations Iberian Flora 
+# Phylogenetic manipulations Iberian flora --------------------------------
+
 library(phytools)
 library(tidyverse)
 library(remotes)
 library(SpeciesAge) #install_github("thauffe/SpeciesAge")
 library(picante)
 
-
-## PENINSULA
-
-### Reading peninsula iucn data
-peninsula <- read_csv(file = "Data/Processed/peninsula_iucn.csv")
-
-### String from the peninsula species name
-peninsula_sp_names <- str_replace(peninsula$species, " ", "_")
-
-### Eliminate subspecies
-peninsula_without_sub <- unique(str_replace(peninsula_sp_names, " .*", ""))
-
 ### Reading mega plant phylogeny#############
 plant_phylo <- read.tree("Data/Raw/PhytoPhylo.tre")
 
-### Eliminating species not present in phylogeny
-n_pen <- peninsula_without_sub[peninsula_without_sub %in% plant_phylo$tip.label]
+###collapsing the phylogeny to genera
 
-### Pruning tree preserving peninsula sp
-peninsula_phy <- keep.tip(plant_phylo, n_pen)
+# obtaining genera name
+genera <- sapply(strsplit(plant_phylo$tip.label, "_"), `[`, 1)
 
-### Saving tree
-save(peninsula_phy, file = "Data/Processed/peninsula_phy.RData")
-
-### Determining the proportion of threatened and non-threatened species
-peninsula_genus <- peninsula %>% group_by(Genus) %>%
-  summarize(
-    richness = n(),
-    threatened_species = sum(threat_category == "threatened"),
-    proportion_threatened = threatened_species / richness
-  )
-
-
-## Implementing phylogenetic age analyses 
-
-### Upscaling to the genus level
-#### obtaining genera name
-genera <- sapply(strsplit(peninsula_phy$tip.label, "_"), `[`, 1)
-
-#### Create a mapping from genus to their corresponding species tips
-genus_map <- split(peninsula_phy$tip.label, genera)
+# Create a mapping from genus to their corresponding species tips
+genus_map <- split(plant_phylo$tip.label, genera)
 
 ### Upscaling with the function
 source("Scripts/functions.R")
-genus_phylo <- collapse_to_genus(peninsula_phy, genus_map) # collapse_to_genus inside the function R.file
-
-### Estimating phylogenetic age without correction
-genus_age <- calculateTipAges(genus_phylo)
+genus_phylo <- collapse_to_genus(plant_phylo, genus_map) # collapse_to_genus inside the function R.file
 
 ### Force in ultrametric
 genus_phylo <- force.ultrametric(genus_phylo)
 
-### Calculating evolutionary distinctiviness among genera
-ev.distinc.peninsula <- evol.distinct(genus_phylo) %>%
-  rename(Genus = Species,
-         ev.dist = w)
+##resolving polytomies
+genus_phylo <- multi2di(genus_phylo)
 
+### Estimating phylogenetic age without correction
+genus_age <- calculateTipAges(genus_phylo)
+
+##saving genus_phylo
+save(genus_phylo, file = "Data/Processed/plant_genus_phylo.RData")
 
 # calculating speciation and extinction rates
 
@@ -108,74 +78,27 @@ for (i in 1:n) {
   
 }
 
+#naming lists according to extinction
 names(list_ages_corrected) <- c("low_ex", "int_ex", "high_ex")
 
 ##collapsing list into a dataframe
-peninsula_ages_rates <- bind_rows(list_ages_corrected,
+plant_ages_corrected <- bind_rows(list_ages_corrected,
                                   .id = "ext_fraction") %>%
-                                  rename(Genus = tip)
-
-##merging with peninsula_genus and evo distinctiveness
-peninsula_ages_total <- left_join(peninsula_ages_rates, peninsula_genus,
-                                  by = "Genus") %>%
-  left_join(ev.distinc.peninsula,
-            by = "Genus") %>% #calculating speciation rate
-  mutate(lambda_a = log(richness)/age,
-         lambda_mean = log(richness)/mean_age)
-
+                         rename(Genus = tip)
 
 ##saving
-write_csv(peninsula_ages_total, file = "Data/Processed/peninsula_ages_total.csv")
+write_csv(plant_ages_corrected,
+          file = "Data/Processed/plant_ages_corrected.csv")
 
 
-##calculating Bloomberg's K and Pagel's lambda
-##filtering data
-data <- peninsula_genus[peninsula_genus$Genus %in% genus_phylo$tip.label, ]
+# Peninsula ---------------------------------------------------------------
 
-# reorder data
-data <- data[match(genus_phylo$tip.label, data$Genus), ]
-
-# extracting the proportion of threatened species
-threatened_vector <- data$proportion_threatened
-
-names(threatened_vector) <- data$Genus
-
-# Calculate Blomberg's K or Pagel's lambda
-K_result <- phylosig(genus_phylo, threatened_vector,
-                     nsim = 100, test = TRUE, method = "K")
-
-lambda_result <- phylosig(genus_phylo,
-                          threatened_vector, nsim = 100,
-                          test = TRUE, method = "lambda")
+### Reading peninsula iucn data
+peninsula <- read_csv(file = "Data/Processed/peninsula_iucn.csv")
 
 
-#########################ANDALUCIA##############################################
-
-
-##reading andalucia iucn data
-andalucia <- read_csv(file = "Data/Processed/andalucia_iucn.csv")
-
-##string from the andalucia species name
-andalucia_sp_names <- str_replace(andalucia$species, " ", "_")
-
-## eliminate subspecies
-andalucia_without_sub <- unique(str_replace(andalucia_sp_names, " .*", ""))
-
-##reading mega plant phylogeny#############
-plant_phylo <- read.tree("Data/Raw/PhytoPhylo.tre")
-
-##eliminating species not present in phylogeny
-n_and <- andalucia_without_sub[andalucia_without_sub %in% plant_phylo$tip.label]
-
-##pruning tree preserving andalucia sp
-andalucia_phy <- keep.tip(plant_phylo, n_and)
-
-##saving
-save(andalucia_phy, file = "Data/Processed/andalucia_phy.RData")
-
-
-##determining the proportion of threatened and non-threatened species
-andalucia_genus <- andalucia %>% group_by(Genus) %>%
+### Determining the proportion of threatened and non-threatened species
+peninsula_genus <- peninsula %>% group_by(Genus) %>%
   summarize(
     richness = n(),
     threatened_species = sum(threat_category == "threatened"),
@@ -183,110 +106,86 @@ andalucia_genus <- andalucia %>% group_by(Genus) %>%
   )
 
 
-# implementing phylogenetic age analyses ----------------------------------
+## filtering in the genus age dataset only the genera present in the Peninsula
+peninsula_ages <- plant_ages_corrected %>% 
+  filter(Genus %in% peninsula_genus$Genus)
 
-##upscaling to the genus level
-#obtaining genera name
-genera <- sapply(strsplit(andalucia_phy$tip.label, "_"), `[`, 1)
-
-# Create a mapping from genus to their corresponding species tips
-genus_map <- split(andalucia_phy$tip.label, genera)
-
-##upscaling with the function
-genus_phylo <- collapse_to_genus(andalucia_phy, genus_map)
-
-##estimating phylogenetic age without correction
-genus_age <- calculateTipAges(genus_phylo)
-
-##transforming in ultrametric
-genus_phylo <- force.ultrametric(genus_phylo)
-
-##calculating evolutionary distinctiviness among genera
-ev.distinc.andalucia <- evol.distinct(genus_phylo) %>%
-  rename(Genus = Species,
-         ev.dist = w)
-
-
-########## calculating speciation and extinction rates########
-
-##extinction fraction vector
-ep.v <- c(0.1, 0.5, 0.9)
-
-
-##empty dataframe
-div.rates <- data.frame(ep.fr = ep.v,
-                        rho.fr = rep(0.85, 3),
-                        lambda = c(0,0,0),
-                        mu = c(0,0,0))
-
-
-##for loop estimating diversification rates
-for (i in 1:nrow(div.rates)) {
-  
-  div.rates[i, 3:4] <- estimateBD(genus_phylo, epsilon = div.rates[i, 1],
-                                  rho = div.rates[i, 2])
-  
-}
-
-##correcting species ages
-
-#empty list for storing information
-list_ages_corrected <- vector(mode = "list", length = nrow(div.rates))
-
-
-##for loop to estimate meanAge for each genus in each list
-for (i in 1:nrow(div.rates)) {
-  
-  list_ages_corrected[[i]] <- data.frame(genus_age,
-                                         mean_age = vector(mode = "numeric",
-                                                           length = nrow(genus_age)))  
-  for (j in 1:nrow(genus_age)) {
-    
-    list_ages_corrected[[i]][j,3] <- meanAge(genus_age[j,2],
-                                             lambda = div.rates[i, 3],
-                                             mu = div.rates[i,4],
-                                             rho = div.rates[i,2])
-    
-  }
-  
-}
-
-names(list_ages_corrected) <- c("low_ex", "int_ex", "high_ex")
-
-##collapsing list into a dataframe
-andalucia_ages_rates <- bind_rows(list_ages_corrected,
-                                  .id = "ext_fraction") %>%
-                                 rename(Genus = tip)
-
-##merging with andalucia_genus and evo distinctiveness
-andalucia_ages_total <- left_join(andalucia_ages_rates, andalucia_genus,
-                                  by = "Genus") %>%
-  left_join(ev.distinc.andalucia,
-            by = "Genus") %>% #calculating speciation rate
-  mutate(lambda_a = log(richness)/age,
-         lambda_mean = log(richness)/mean_age)
-
+## left joining peninsula ages and proportion data
+peninsula_ages_proportion <- left_join(peninsula_ages, peninsula_genus,
+                                       by = "Genus")
 
 ##saving
-write_csv(andalucia_ages_total, file = "Data/Processed/andalucia_ages_total.csv")
+write_csv(peninsula_ages_proportion, 
+          file = "Data/Processed/peninsula_ages_proportion.csv")
 
 
 ##calculating Bloomberg's K and Pagel's lambda
+
 ##filtering data
-data <- andalucia_genus[andalucia_genus$Genus %in% genus_phylo$tip.label, ]
+peninsula_signal <- peninsula_genus[peninsula_genus$Genus %in% genus_phylo$tip.label, ]
 
 # reorder data
-data <- data[match(genus_phylo$tip.label, data$Genus), ]
+peninsula_signal <- peninsula_signal[match(genus_phylo$tip.label,
+                                           peninsula_signal$Genus), ]
 
 # extracting the proportion of threatened species
-threatened_vector <- data$proportion_threatened
+threatened_peninsula <- peninsula_signal$proportion_threatened
 
-names(threatened_vector) <- data$Genus
+names(threatened_peninsula) <- peninsula_signal$Genus
 
 # Calculate Blomberg's K and Pagel's lambda
-K_result_and <- phylosig(genus_phylo, threatened_vector,
-                         nsim = 100, test = TRUE, method="K")
+K_peninsula <- phylosig(genus_phylo, threatened_peninsula,
+                        nsim = 100, test = TRUE, method="K")
 
-lambda_result_and <- phylosig(genus_phylo,
-                          threatened_vector, nsim = 100,
-                          test = TRUE, method="lambda")
+lambda_peninsula <- phylosig(genus_phylo,
+                             threatened_peninsula, nsim = 100,
+                             test = TRUE, method="lambda")
+
+# Andalusia ---------------------------------------------------------------
+
+##reading andalucia iucn data
+andalucia <- read_csv(file = "Data/Processed/andalucia_iucn.csv")
+
+
+### Determining the proportion of threatened and non-threatened species
+andalucia_genus <- andalucia %>% group_by(Genus) %>%
+  summarize(
+    richness = n(),
+    threatened_species = sum(threat_category == "threatened"),
+    proportion_threatened = threatened_species / richness
+  )
+
+## filtering in the genus age dataset only the genera present in the Peninsula
+andalucia_ages <- plant_ages_corrected %>% 
+  filter(Genus %in% andalucia_genus$Genus)
+
+## left joining peninsula ages and proportion data
+andalucia_ages_proportion <- left_join(andalucia_ages, andalucia_genus,
+                                       by = "Genus")
+
+#saving dataset
+write_csv(andalucia_ages_proportion,
+          file = "Data/Processed/andalucia_ages_proportion.csv")
+
+
+##calculating Bloomberg's K and Pagel's lambda
+
+##filtering data
+andalucia_signal <- andalucia_genus[andalucia_genus$Genus %in% genus_phylo$tip.label, ]
+
+# reorder data
+andalucia_signal <- andalucia_signal[match(genus_phylo$tip.label,
+                                           andalucia_signal$Genus), ]
+
+# extracting the proportion of threatened species
+threatened_andalucia <- andalucia_signal$proportion_threatened
+
+names(threatened_andalucia) <- andalucia_signal$Genus
+
+# Calculate Blomberg's K and Pagel's lambda
+K_andalucia <- phylosig(genus_phylo, threatened_andalucia,
+                        nsim = 100, test = TRUE, method="K")
+
+lambda_andalucia <- phylosig(genus_phylo,
+                             threatened_andalucia, nsim = 100,
+                             test = TRUE, method="lambda")
